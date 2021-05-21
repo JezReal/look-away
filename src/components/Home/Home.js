9b473a56dfb360e128c3cf972c6701cb0a8c1b4f
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-05-22 02:09:49 +0800

    move timer to its own page component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import Timer from "../Timer/Timer";
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div id="app-container">
+            <h1>Look away</h1>
+            <hr/>
+
+            <Timer/>
+
+            <div id="footer">
+                <Link to="/information" target="_blank" rel="noreferrer">Why do I need to look away?</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
