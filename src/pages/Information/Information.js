867366df8746f239ecb08057b4da1235db79d0a0
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-05-29 18:06:41 +0800

    update content of information page

--- src/pages/Information/Information.js
+++ src/pages/Information/Information.js
@@ -13,12 +13,10 @@ const Information = () => {
                     </h3>
 
                     <p className="article-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam ornare consequat. Vestibulum dignissim aliquam magna,
-                        sed elementum diam rutrum non. Nullam quis nisi ac dolor sollicitudin pellentesque. In pharetra, sapien non pellentesque
-                        feugiat, ligula est dictum libero, at blandit magna massa eget ligula. Nullam hendrerit est eget blandit laoreet. Sed
-                        vulputate urna dui, nec convallis augue rutrum ac. Integer nec justo et nunc viverra porttitor. Maecenas nulla elit,
-                        hendrerit vel bibendum vestibulum, venenatis et velit. Pellentesque ex neque, egestas in urna eu, dictum tempor diam.
-                        Sed convallis gravida dui in bibendum. Praesent a commodo tellus.
+                        As everyone is now working or learning from home, the time that we spend staring at screens have been greater than ever.
+                        Naturally, this comes with it a lot of potential problems. One of these is digital eye strain. <a href="https://www.aoa.org/healthy-eyes/eye-and-vision-conditions/computer-vision-syndrome?sso=y" target="_blank" rel="noreferrer">Digital eye strain</a>
+                        describes a group of eye and vision-related problems that arise from prolonged use of digital devices.
+                        With this, we thought of creating a web app that can help in minimizing digital eye strain which gave birth to look away.
                     </p>
                 </div>
 
@@ -29,13 +27,10 @@ const Information = () => {
                     </h3>
 
                     <p className="article-content">
-                        Curabitur ut eros vel sapien efficitur interdum id a magna. Suspendisse at vehicula metus. In non ex diam.
-                        Suspendisse rutrum, velit a molestie rhoncus, erat nunc pretium metus, at pretium nibh tellus sit amet arcu.
-                        Nullam eget nulla sed ante scelerisque maximus tempor malesuada nisl. Nam non augue non urna molestie accumsan.
-                        Curabitur ultrices sed nulla ac maximus. Sed eu velit id urna convallis iaculis non in mauris. Aenean sit amet imperdiet
-                        augue. Cras at vulputate ex, ut volutpat dui. Nullam sed posuere dolor. Vestibulum congue nunc lacinia suscipit tincidunt.
-                        Sed condimentum quam eu purus ultricies, vitae porttitor augue tristique. Vestibulum efficitur ex sed enim dictum hendrerit.
-                        Integer luctus leo nisl, eu tincidunt metus rhoncus vitae.
+                        According to <a href="https://www.healthline.com/health/eye-health/20-20-20-rule" target="_blank" rel="noreferrer">healthline.com</a>, 20-20-20 rule is a general rule that you can follow if you find yourself staring at screens for a really long period of time.
+                        The general idea is that for every 20 minutes you spend staring at a screen, you should try to look away at something 20 feet away for 20 seconds.
+                        How are you going to look at something 20 feet away? Well, you shouldn't be exact with the measurement the key is to look at something
+                        that is far away from you. Finally, the reason for 20 seconds is that it takes about 20 seconds for your eyes to completely relax.
                     </p>
                 </div>
 
@@ -46,11 +41,10 @@ const Information = () => {
                     </h3>
 
                     <p className="article-content">
-                        Fusce consectetur fringilla tincidunt. Pellentesque laoreet venenatis eros, id elementum lacus sollicitudin a.
-                        Suspendisse fermentum mi at leo condimentum vestibulum. Suspendisse et tortor et ipsum viverra dapibus non a diam.
-                        Suspendisse a blandit orci. Etiam in odio in nulla aliquet placerat. Sed tincidunt ipsum sed risus dictum, eget rhoncus
-                        urna rutrum. Sed enim orci, sodales at elit sed, vehicula aliquet urna. Suspendisse aliquet, ipsum non dapibus consectetur,
-                        purus libero interdum ex, vitae placerat metus sem sit amet tellus. Quisque interdum accumsan ante, et lobortis velit efficitur ut.
+                        Aside from following the 20-20-20 rule, there are other ways we can reduce digital eye strain one of them is adjusting your
+                        computer display settings such as brightness, text size, contrast and color temperature. Another way is to use computer glasses
+                        and/or using eye drops. Finally, you can also blink more often when looking at a screen because it helps moisten your eyes to
+                        prevent dryness and irritation.
                     </p>
                 </div>
             </div>
